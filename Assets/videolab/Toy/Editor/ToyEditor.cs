dc861407cad073b4bc0279cb99e1ba2f856c0524
finnTE <finn@teenage.engineering>
2017-11-22 12:34:17 +0100

    Toy fix

--- Assets/videolab/Toy/Editor/ToyEditor.cs
+++ Assets/videolab/Toy/Editor/ToyEditor.cs
@@ -3,20 +3,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace VideoLab
 {
     [CanEditMultipleObjects]
     [CustomEditor(typeof(Toy))]
     public class ToyEditor : Editor {
+        
+        const string kTemplatePath = "Assets/videolab/Toy/Shader/Toy.shader";
+
+        SerializedProperty _shadertoyText;
+        SerializedProperty _shader;
+
+        void OnEnable()
+        {
+            _shadertoyText  = serializedObject.FindProperty("_shadertoyText");
+            _shader         = serializedObject.FindProperty("_shader");
+        }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            DrawPropertiesExcluding(serializedObject, new string[] {"m_Script"});
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(_shadertoyText);
+            if (EditorGUI.EndChangeCheck())
+            {
+                ConvertShader((TextAsset)_shadertoyText.objectReferenceValue);
+            }
+
+            EditorGUILayout.Space();
+
+            DrawPropertiesExcluding(serializedObject, new string[] {"m_Script", "_shadertoyText"});
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        void ConvertShader(TextAsset shadertoyText)
+        {
+            if (shadertoyText == null)
+            {
+                _shader.objectReferenceValue = (Shader)AssetDatabase.LoadAssetAtPath(kTemplatePath, typeof(Shader));
+                return;
+            }
+
+            string stPath = AssetDatabase.GetAssetPath(shadertoyText);
+            string template = File.ReadAllText(kTemplatePath);
+
+            // Rename shader.
+            template = template.Replace("Hidden/Toy", "Hidden/" + Path.GetFileNameWithoutExtension(stPath));
+
+            // Insert Shadertoy code.
+            template = template.Replace("fixed4 mainImage( float2 fragCoord ) { return fixed4(0, 0, 0, 0); }", shadertoyText.text);
+
+            // Rewrite entry point signature.
+            template = template.Replace("void mainImage( out vec4 fragColor, in vec2 fragCoord )", "fixed4 mainImage( float2 fragCoord )");
+            template = template.Replace("fragColor =", "return");
+
+            template = template.Replace("gl_FragCoord", "fragCoord");
+            template = template.Replace("iResolution", "_ScreenParams");
+            template = template.Replace("iTime", "_Time.y");
+            template = template.Replace("iTimeDelta", "unity_DeltaTime.x");
+
+            template = template.Replace("const", "static const");
+
+            template = template.Replace("mix(", "lerp(");
+            template = template.Replace("fract(", "frac(");
+            template = template.Replace("mod(", "fmod(");
+            template = template.Replace("dFdx(", "ddx(");
+            template = template.Replace("dFdy(", "ddy(");
+            template = template.Replace("fma(", "mad(");
+            template = template.Replace("texture(", "tex2D(");
+            template = template.Replace("texture2D(", "tex2D(");
+
+            template = Regex.Replace(template, @"vec([2-4])", "float$1");
+            template = Regex.Replace(template, @"ivec([2-4])", "int$1");
+            template = Regex.Replace(template, @"bvec([2-4])", "bool$1");
+            template = Regex.Replace(template, @"float4\(([^(,]+?)\)", "float4($1, $1, $1, $1)");
+            template = Regex.Replace(template, @"float3\(([^(,]+?)\)", "float3($1, $1, $1)");
+            template = Regex.Replace(template, @"float2\(([^(,]+?)\)", "float2($1, $1)");
+
+            template = Regex.Replace(template, @"mat([2-4])", "float$1x$1");
+            template = Regex.Replace(template, @"([\w.]+)\s*\*=\s*([^;]+)", "$1 = mul($2, $1)");
+
+            foreach (Match match in Regex.Matches(template, @"textureLod\((.*)\)"))
+            {
+                var args = Regex.Matches(match.Groups[1].Value, @"(?:[^,()]+((?:\((?>[^()]+|\((?<open>)|\)(?<-open>))*\)))*)+");
+                string rep = String.Format("tex2Dlod({0}, float4(({1}).x, ({1}).y, 0., {2}))", args[0], args[1], args[2]);
+                template = template.Replace(match.Value, rep);
+            }
+
+            foreach (Match match in Regex.Matches(template, @"atan\((.*)\)"))
+            {
+                var args = Regex.Matches(match.Groups[1].Value, @"(?:[^,()]+((?:\((?>[^()]+|\((?<open>)|\)(?<-open>))*\)))*)+");
+                string rep = String.Format("atan2({1}, {0})", args[0], args[1]);
+                template = template.Replace(match.Value, rep);
+            }
+
+            string shaderPath = Path.GetDirectoryName(stPath) + "/" + Path.GetFileNameWithoutExtension(stPath) + ".shader";
+            File.WriteAllText(shaderPath, template);
+            AssetDatabase.Refresh();
+
+            _shader.objectReferenceValue = (Shader)AssetDatabase.LoadAssetAtPath(shaderPath, typeof(Shader));
         }
     }
 }
--- Assets/videolab/Toy/Toy.cs
+++ Assets/videolab/Toy/Toy.cs
@@ -1,26 +1,22 @@
 using UnityEngine;
-using UnityEditor;
-using System.IO;
-using System.Text.RegularExpressions;
 using System;
 
 namespace VideoLab
 {
-    [ExecuteInEditMode]
     [RequireComponent(typeof(Camera))]
     [AddComponentMenu("Image Effects/Toy")]
     public class Toy : MonoBehaviour
     {
-        const string kTemplatePath = "Assets/videolab/Toy/Shader/Toy.shader";
-
         #region Public Properties
 
-        TextAsset _prevShadertoyText;
-        public TextAsset shadertoyText;
+        [SerializeField]
+        TextAsset _shadertoyText;
 
-        public Vector2 _resolution = new Vector2(1280, 720);
+        [SerializeField]
+        Vector2 _resolution = new Vector2(1280, 720);
 
-        public Texture[] channels = new Texture[4];
+        [SerializeField]
+        Texture[] _channels = new Texture[4];
 
         #endregion
 
@@ -35,79 +31,12 @@ namespace VideoLab
 
         Vector2 _mouseDown = Vector2.zero;
 
-        void ConvertShader()
-        {
-            string stCode = shadertoyText.text;
-            string stPath = AssetDatabase.GetAssetPath(shadertoyText);
-
-            string template = File.ReadAllText(kTemplatePath);
-
-            // Rename shader.
-            template = template.Replace("Hidden/Toy", "Hidden/" + Path.GetFileNameWithoutExtension(stPath));
-
-            // Insert Shadertoy code.
-            template = template.Replace("fixed4 mainImage( float2 fragCoord ) { return fixed4(0, 0, 0, 0); }", stCode);
-
-            // Rewrite entry point signature.
-            template = template.Replace("void mainImage( out vec4 fragColor, in vec2 fragCoord )", "fixed4 mainImage( float2 fragCoord )");
-            template = template.Replace("fragColor =", "return");
-
-            template = template.Replace("gl_FragCoord", "fragCoord");
-            template = template.Replace("iResolution", "_ScreenParams");
-            template = template.Replace("iTime", "_Time.y");
-            template = template.Replace("iTimeDelta", "unity_DeltaTime.x");
-
-            template = template.Replace("const", "static const");
-
-            template = template.Replace("mix(", "lerp(");
-            template = template.Replace("fract(", "frac(");
-            template = template.Replace("mod(", "fmod(");
-            template = template.Replace("dFdx(", "ddx(");
-            template = template.Replace("dFdy(", "ddy(");
-            template = template.Replace("fma(", "mad(");
-            template = template.Replace("texture(", "tex2D(");
-            template = template.Replace("texture2D(", "tex2D(");
-
-            template = Regex.Replace(template, @"vec([2-4])", "float$1");
-            template = Regex.Replace(template, @"ivec([2-4])", "int$1");
-            template = Regex.Replace(template, @"bvec([2-4])", "bool$1");
-            template = Regex.Replace(template, @"float4\(([^(,]+?)\)", "float4($1, $1, $1, $1)");
-            template = Regex.Replace(template, @"float3\(([^(,]+?)\)", "float3($1, $1, $1)");
-            template = Regex.Replace(template, @"float2\(([^(,]+?)\)", "float2($1, $1)");
-
-            template = Regex.Replace(template, @"mat([2-4])", "float$1x$1");
-            template = Regex.Replace(template, @"([\w.]+)\s*\*=\s*([^;]+)", "$1 = mul($2, $1)");
-
-            foreach (Match match in Regex.Matches(template, @"textureLod\((.*)\)"))
-            {
-                var args = Regex.Matches(match.Groups[1].Value, @"(?:[^,()]+((?:\((?>[^()]+|\((?<open>)|\)(?<-open>))*\)))*)+");
-                string rep = String.Format("tex2Dlod({0}, float4(({1}).x, ({1}).y, 0., {2}))", args[0], args[1], args[2]);
-                template = template.Replace(match.Value, rep);
-            }
-
-            foreach (Match match in Regex.Matches(template, @"atan\((.*)\)"))
-            {
-                var args = Regex.Matches(match.Groups[1].Value, @"(?:[^,()]+((?:\((?>[^()]+|\((?<open>)|\)(?<-open>))*\)))*)+");
-                string rep = String.Format("atan2({1}, {0})", args[0], args[1]);
-                template = template.Replace(match.Value, rep);
-            }
-
-            string shaderPath = Path.GetDirectoryName(stPath) + "/" + Path.GetFileNameWithoutExtension(stPath) + ".shader";
-            File.WriteAllText(shaderPath, template);
-            AssetDatabase.Refresh();
-
-            _shader = (Shader)AssetDatabase.LoadAssetAtPath(shaderPath, typeof(Shader));
-        }
-
         #endregion
 
         #region MonoBehaviour Functions
 
         void Start()
         {
-            if (shadertoyText == null)
-                _shader = (Shader)AssetDatabase.LoadAssetAtPath(kTemplatePath, typeof(Shader));
-
             _frameCounter = 0;
         }
 
@@ -132,10 +61,10 @@ namespace VideoLab
             _material.SetFloatArray("iChannelTime", channelTime);
 
             Vector4[] channelResolution = new Vector4[4];
-            channelResolution[0] = (channels[0] != null) ? new Vector4(channels[0].width, channels[0].height): new Vector4();
-            channelResolution[1] = (channels[1] != null) ? new Vector4(channels[1].width, channels[1].height): new Vector4();
-            channelResolution[2] = (channels[2] != null) ? new Vector4(channels[2].width, channels[2].height): new Vector4();
-            channelResolution[3] = (channels[3] != null) ? new Vector4(channels[3].width, channels[3].height): new Vector4();
+            channelResolution[0] = (_channels[0] != null) ? new Vector4(_channels[0].width, _channels[0].height): new Vector4();
+            channelResolution[1] = (_channels[1] != null) ? new Vector4(_channels[1].width, _channels[1].height): new Vector4();
+            channelResolution[2] = (_channels[2] != null) ? new Vector4(_channels[2].width, _channels[2].height): new Vector4();
+            channelResolution[3] = (_channels[3] != null) ? new Vector4(_channels[3].width, _channels[3].height): new Vector4();
             _material.SetVectorArray("iChannelResolution", channelResolution);
 
             Vector2 mousePos = Vector2.zero;
@@ -150,10 +79,10 @@ namespace VideoLab
 
             _material.SetFloat("iSampleRate", 44.100f);
 
-            _material.SetTexture("iChannel0", channels[0]);
-            _material.SetTexture("iChannel1", channels[1]);
-            _material.SetTexture("iChannel2", channels[2]);
-            _material.SetTexture("iChannel3", channels[3]);
+            _material.SetTexture("iChannel0", _channels[0]);
+            _material.SetTexture("iChannel1", _channels[1]);
+            _material.SetTexture("iChannel2", _channels[2]);
+            _material.SetTexture("iChannel3", _channels[3]);
 
             RenderTexture rt = source;
             rt = RenderTexture.GetTemporary((int)_resolution.x, (int)_resolution.y);
@@ -165,19 +94,6 @@ namespace VideoLab
             RenderTexture.ReleaseTemporary(rt);
 
             ++_frameCounter;
-        }
-
-        void OnValidate()
-        {
-            if (shadertoyText != _prevShadertoyText)
-            {
-                _prevShadertoyText = shadertoyText;
-
-                if (shadertoyText != null)
-                    ConvertShader();
-                else
-                    _shader = (Shader)AssetDatabase.LoadAssetAtPath(kTemplatePath, typeof(Shader));
-            }
         }
 
         #endregion
