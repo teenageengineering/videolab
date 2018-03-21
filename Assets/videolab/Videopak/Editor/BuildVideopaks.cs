726c5802c879570428d5699b658436d42f92319d
finn <finn@teenage.engineering>
2018-03-21 15:30:05 +0100

    Videopak!

---
+++ Assets/videolab/Videopak/Editor/BuildVideopaks.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildVideopaks
+{
+    [MenuItem ("Assets/Build Videopaks")]
+    static void BuildAllAssetBundles()
+    {
+        string dir = "videopaks/";
+        
+        foreach (string bundleName in AssetDatabase.GetAllAssetBundleNames())
+        {
+            AssetBundleBuild buildInfo = new AssetBundleBuild();
+            buildInfo.assetBundleName = bundleName;
+            buildInfo.assetNames = AssetDatabase.GetAssetPathsFromAssetBundle(bundleName);
+            AssetBundleBuild[] buildMap = new AssetBundleBuild[] { buildInfo };
+
+            string osxDir = dir + bundleName + "/" + VideopakManager.GetPlatformString(RuntimePlatform.OSXPlayer);
+            Directory.CreateDirectory(osxDir);
+            BuildPipeline.BuildAssetBundles(osxDir, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+
+            string iosDir = dir + bundleName + "/" + VideopakManager.GetPlatformString(RuntimePlatform.IPhonePlayer);
+            Directory.CreateDirectory(iosDir);
+            BuildPipeline.BuildAssetBundles(iosDir, buildMap, BuildAssetBundleOptions.None, BuildTarget.iOS);
+        }
+    }
+}
---
+++ Assets/videolab/Videopak/VideopakManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class VideopakManager
+{
+    AssetBundle _bundle;
+
+    static VideopakManager _instance;
+
+    public static VideopakManager Instance {
+        get {
+            if (_instance == null)
+                _instance = new VideopakManager();
+
+            return _instance;
+        }
+    }
+
+    public static string GetPlatformString(RuntimePlatform platform)
+    {
+        if (platform == RuntimePlatform.IPhonePlayer)
+            return "iOS";
+
+        if (platform == RuntimePlatform.OSXEditor || platform == RuntimePlatform.OSXPlayer)
+            return "OSX";
+
+        return null;
+    }
+
+    public static void LoadPak(string pakName)
+    {
+        Unload();
+
+        string platform = GetPlatformString(Application.platform);
+        if (platform == null)
+        {
+            Debug.Log("[VideopakManager] Unsupported platform");
+            return;
+        }
+
+        string path = Application.persistentDataPath + "/videopaks/" + pakName + "/" + platform + "/" + pakName;
+
+        AssetBundle bundle = AssetBundle.LoadFromFile(path);
+        if (bundle == null)
+        {
+            Debug.Log("[VideopakManager] Failed to load videopak " + path);
+            return;
+        }
+
+        var scenePaths = bundle.GetAllScenePaths();
+        if (scenePaths.Length == 0)
+        {
+            Debug.Log("[VideopakManager] No scene to load");
+            return;
+        }
+
+        SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Additive);
+
+        Instance._bundle = bundle;
+    }
+
+    public static void Unload()
+    {
+        AssetBundle bundle = Instance._bundle;
+
+        if (bundle != null)
+        {
+            var scenePaths = bundle.GetAllScenePaths();
+            SceneManager.UnloadScene(scenePaths[0]);
+
+            bundle.Unload(true);
+        }
+    }
+}
