be0a64a5b3d8301ff215576178ce3e4e82abfb5e
finn <finn@teenage.engineering>
2018-03-23 13:08:58 +0100

    Add RigidbodyOut

---
+++ Assets/Klak/Wiring/Editor/Output/RigidbodyOutEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Wiring
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(RigidbodyOut))]
+    public class RigidbodyOutEditor : Editor
+    {
+        SerializedProperty _rigidbody;
+        SerializedProperty _forceMode;
+        SerializedProperty _useLocalPOI;
+
+        void OnEnable()
+        {
+            _rigidbody = serializedObject.FindProperty("_rigidbody");
+            _forceMode = serializedObject.FindProperty("_forceMode");
+            _useLocalPOI = serializedObject.FindProperty("_useLocalPOI");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_rigidbody);
+            EditorGUILayout.PropertyField(_forceMode);
+            EditorGUILayout.PropertyField(_useLocalPOI);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Klak/Wiring/Output/RigidbodyOut.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Output/Component/Rigidbody Out")]
+    public class RigidbodyOut : NodeBase
+    {
+        #region Editable properties
+
+        [SerializeField]
+        Rigidbody _rigidbody;
+
+        [SerializeField]
+        ForceMode _forceMode = ForceMode.Force;
+
+        [SerializeField]
+        bool _useLocalPOI;
+
+        #endregion
+
+        Vector3 _force;
+        Vector3 _position;
+
+        #region Node I/O
+
+        [Inlet]
+        public Vector3 force {
+            set {
+                _force = value;
+            }
+        }
+
+        [Inlet]
+        public Vector3 pointOfImpact {
+            set {
+                _position = value;
+            }
+        }
+
+        [Inlet]
+        public void Bang()
+        {
+            Vector3 position = (_useLocalPOI) ? _rigidbody.centerOfMass + _position : _position;
+            _rigidbody.AddForceAtPosition(_force, position, _forceMode);
+        }
+
+        #endregion
+    }
+}
