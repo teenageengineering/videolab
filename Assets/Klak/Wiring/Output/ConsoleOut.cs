da0c985ae63ffe5350c5eeb538d8e665508121e6
finn <finn@teenage.engineering>
2018-04-06 11:38:25 +0200

    Add ConsoleOut Klak node

---
+++ Assets/Klak/Wiring/Output/ConsoleOut.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Reflection;
+
+namespace Klak.Wiring
+{
+	[AddComponentMenu("Klak/Wiring/Output/Generic/Console Out")]
+	public class ConsoleOut : NodeBase
+	{
+		#region Node I/O
+
+		[Inlet]
+		public void Bang() {
+			Debug.Log(name + " Bang!");
+		}
+
+		[Inlet]
+		public float number {
+			set {
+				Debug.Log(name + " " + value);
+			}
+		}
+
+		[Inlet]
+		public Vector3 vector {
+			set {
+				Debug.Log(name + " Vector3" + value);
+			}
+		}
+
+		[Inlet]
+		public Quaternion rotation {
+			set {
+				Debug.Log(name + " Quaternion" + value);
+			}
+		}
+
+		[Inlet]
+		public Color color {
+			set {
+				Debug.Log(name + " " + value);
+			}
+		}
+
+		#endregion
+	}
+}
