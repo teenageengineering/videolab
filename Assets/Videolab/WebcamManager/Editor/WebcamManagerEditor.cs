19dcca4d812243222da06e8e2a933783740672d3
finn <finn@teenage.engineering>
2018-04-20 21:26:45 +0200

    Proper webcam manager

---
+++ Assets/Videolab/WebcamManager/Editor/WebcamManagerEditor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+namespace VideoLab
+{
+    [CustomEditor(typeof(WebcamManager), true)]
+    public class WebcamManagerEditor : Editor 
+    {
+        public override void OnInspectorGUI()
+        {
+            WebcamManager manager = target as WebcamManager;
+
+            List<string> deviceNames = new List<string>();
+
+            deviceNames.Add("No device");
+
+            foreach (WebCamDevice dev in WebCamTexture.devices)
+                deviceNames.Add(dev.name);
+
+            manager.deviceIndex = EditorGUILayout.Popup("Device", manager.deviceIndex + 1, deviceNames.ToArray()) - 1;
+
+            EditorGUILayout.Space();
+
+            manager.material = (Material)EditorGUILayout.ObjectField("Material", manager.material, typeof(Material), true);
+
+            EditorGUILayout.Space();
+
+            manager.rawImage = (RawImage)EditorGUILayout.ObjectField("Raw Image", manager.rawImage, typeof(RawImage), true);
+            manager.aspectRatioFitter = (AspectRatioFitter)EditorGUILayout.ObjectField("Aspect Ratio Fitter", manager.aspectRatioFitter, typeof(AspectRatioFitter), true);
+        }
+    }
+}
---
+++ Assets/Videolab/WebcamManager/WebcamManager.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+
+namespace VideoLab
+{
+    public class WebcamManager : MonoBehaviour
+    {
+        #region Editable properties
+
+        [SerializeField]
+        int _deviceIndex = -1;
+
+        public int deviceIndex {
+            get { return _deviceIndex; }
+            set { 
+                if (value == _deviceIndex)
+                    return;
+                
+                _deviceIndex = value;
+
+                ReloadCamTexture();
+            }
+        }
+
+        [SerializeField]
+        Material _material;
+
+        public Material material {
+            get { return _material; }
+            set {
+                if (value == _material)
+                    return;
+                
+                if (_material)
+                    _material.mainTexture = null;
+
+                _material = value;
+
+                if (_material)
+                    _material.mainTexture = _camTexture;
+            }
+        }
+
+        [SerializeField]
+        RawImage _rawImage;
+
+        public RawImage rawImage {
+            get { return _rawImage; }
+            set {
+                if (value == _rawImage)
+                    return;
+
+                if (_rawImage)
+                    _rawImage.texture = null;
+
+                _rawImage = value;
+
+                if (_rawImage)
+                    _rawImage.texture = _camTexture;
+
+                _frameNeedsFixing = true;
+            }
+        }
+
+        [SerializeField]
+        AspectRatioFitter _aspectRatioFitter;
+
+        public AspectRatioFitter aspectRatioFitter {
+            get { return _aspectRatioFitter; }
+            set {
+                if (value == _aspectRatioFitter)
+                    return;
+                
+                _aspectRatioFitter = value;
+
+                _frameNeedsFixing = true;
+            }
+        }
+
+        #endregion
+
+        #region Public members
+
+        public bool playing {
+            get { return _camTexture.isPlaying; }
+            set {
+                if (_camTexture == null || value == _camTexture.isPlaying)
+                    return;
+                
+                if (value)
+                    _camTexture.Play();
+                else 
+                    _camTexture.Stop();
+            }
+        }
+
+        #endregion
+
+        #region Private
+
+        WebCamTexture _camTexture;
+
+        bool _frameNeedsFixing;
+
+        void ReloadCamTexture()
+        {
+            playing = false;
+
+            _camTexture = null;
+
+            if (_deviceIndex >= 0 && _deviceIndex < WebCamTexture.devices.Length)
+            {
+                WebCamDevice device = WebCamTexture.devices.ElementAt(_deviceIndex);
+                WebCamTexture texture = new WebCamTexture(device.name);
+                _camTexture = texture;
+
+                if (_material)
+                    _material.mainTexture = _camTexture;
+
+                if (_rawImage)
+                    _rawImage.texture = _camTexture;
+
+                _frameNeedsFixing = true;
+            }
+        }
+
+        #endregion
+
+        #region Monobehaviour
+
+        void Start()
+        {
+            ReloadCamTexture();
+        }
+
+        void Update()
+        {
+            if (!_camTexture || _camTexture.width < 100)
+                return;
+
+            if (_frameNeedsFixing)
+            {
+                if (_rawImage)
+                {
+                    _rawImage.rectTransform.localEulerAngles = new Vector3(0, 0, -_camTexture.videoRotationAngle);
+                    _rawImage.uvRect = _camTexture.videoVerticallyMirrored ? new Rect(0, 1, 1, -1) : new Rect(0, 0, 1, 1);
+                }
+
+                if (_aspectRatioFitter)
+                    _aspectRatioFitter.aspectRatio = (float)_camTexture.width / _camTexture.height;
+
+                _frameNeedsFixing = false;
+            }
+        }
+
+        #endregion
+    }
+}
