26bd75a2628a85745c1c3ac7ceff58997db62aca
finn <finn@teenage.engineering>
2018-04-27 15:19:30 +0200

    Add RadioGroupMember
    Assign to a set of objects from which only one can be active at a time.

---
+++ Assets/Videolab/Scripts/RadioGroupMember.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VideoLab
+{
+    public class RadioGroupMember : MonoBehaviour
+    {
+        #region public
+
+        public int groupIndex = 0;
+
+        #endregion
+
+        delegate void RadioGroupDelegate(RadioGroupMember member);
+        static event RadioGroupDelegate OnMemberEnabled;
+
+        int _prevGroupIndex;
+
+        void Awake()
+        {
+            _prevGroupIndex = groupIndex;
+                
+            OnMemberEnabled += onMemberEnabled;
+        }
+
+        void OnDestroy()
+        {
+            OnMemberEnabled -= onMemberEnabled;
+        }
+
+        void OnEnable()
+        {
+            OnMemberEnabled(this);
+        }
+
+        void Update()
+        {
+            if (groupIndex != _prevGroupIndex) {
+                
+                if (gameObject.activeSelf)
+                    OnMemberEnabled(this);
+
+                _prevGroupIndex = groupIndex;
+            }
+        }
+
+        void onMemberEnabled(RadioGroupMember member)
+        {
+            if (member != this && member.groupIndex != this.groupIndex)
+                gameObject.SetActive(false);
+        }
+    }
+}
