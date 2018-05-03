e7f9e63e6f83a31958c99bbf753fd2db27b79546
finn <finn@teenage.engineering>
2018-05-03 11:00:40 +0200

    Add photomatic image effect

---
+++ Assets/Videolab/Photomatic/PhotomaticEffect.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace VideoLab
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Image Effects/Photomatic")]
+    public class PhotomaticEffect : MonoBehaviour
+    {
+        #region Public Properties
+
+        [SerializeField, Range(0, 1)]
+        float _hue = 0;
+
+        public float hue {
+            get { return _hue; }
+            set { _hue = value; }
+        }
+
+        [SerializeField, Range(0, 1)]
+        float _saturation = 0.5f;
+
+        public float saturation {
+            get { return _saturation; }
+            set { _saturation = value; }
+        }
+
+        [SerializeField, Range(0, 1)]
+        float _brightness = 0.5f;
+
+        public float brightness {
+            get { return _brightness; }
+            set { _brightness = value; }
+        }
+
+        [SerializeField, Range(0, 1)]
+        float _contrast = 0.5f;
+
+        public float contrast {
+            get { return _contrast; }
+            set { _contrast = value; }
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        [SerializeField, HideInInspector]
+        Shader _shader;
+
+        Material _material;
+
+        #endregion
+
+        #region MonoBehaviour Functions
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (_material == null)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+
+            _material.SetVector("_hsbc", new Vector4(_hue, _saturation, _brightness, _contrast));
+
+            Graphics.Blit(source, destination, _material);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Videolab/Photomatic/Shader/Photomatic.shader
@@ -0,0 +1,55 @@
+﻿Shader "Hidden/Photomatic"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex vert_img
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+            
+            sampler2D _MainTex;
+
+            float4 _hsbc;
+
+            float3 applyHue(float3 aColor, float aHue)
+			{
+			    float angle = radians(aHue);
+			    float3 k = float3(0.57735, 0.57735, 0.57735);
+			    float cosAngle = cos(angle);
+			    //Rodrigues' rotation formula
+			    return aColor * cosAngle + cross(k, aColor) * sin(angle) + k * dot(k, aColor) * (1 - cosAngle);
+			}
+
+            fixed4 frag(v2f_img i) : SV_Target
+            {
+                float3 src = tex2D(_MainTex, i.uv).rgb;
+
+                float hue = 360 * _hsbc.x;
+			    float saturation = _hsbc.y * 2;
+			    float brightness = _hsbc.z * 2 - 1;
+			    float contrast = _hsbc.w * 2;
+
+			    src = applyHue(src, hue);
+			    src = (src - 0.5f) * contrast + 0.5f;
+			    src = src + brightness;        
+			    float3 intensity = dot(src, float3(0.299,0.587,0.114));
+			    src = lerp(intensity, src, saturation);
+
+                return fixed4(src, 1.0);
+            }
+
+            ENDCG
+        }
+    }
+}
