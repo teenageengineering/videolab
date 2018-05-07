de5af2fb79be3aebd50fe3dc3671d563774929b2
finn <finn@teenage.engineering>
2018-05-07 15:38:56 +0200

    Add some camera scripts

---
+++ Assets/Videolab/Scripts/CameraSnapshot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Camera))]
+public class CameraSnapshot : MonoBehaviour {
+
+    public Vector2 frameSize;
+    public RawImage rawImage;
+    public AspectRatioFitter aspectRatioFitter;
+
+    Camera _camera;
+    RenderTexture _renderTex;
+    Texture2D _tex;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        if (!_camera)
+            return;
+        
+        _renderTex = new RenderTexture((int)frameSize.x, (int)frameSize.y, 24);
+        _tex = new Texture2D((int)frameSize.x, (int)frameSize.y, TextureFormat.RGB24, false);
+    }
+
+    void Snap()
+    {
+        if (!_camera)
+            return;
+
+        _camera.targetTexture = _renderTex;
+        _camera.Render();
+        _camera.targetTexture = null;
+
+        RenderTexture prevRt = RenderTexture.active;
+
+        RenderTexture.active = _renderTex;
+        _tex.ReadPixels(new Rect(0, 0, _tex.width, _tex.height), 0, 0);
+        _tex.Apply();
+
+        if (rawImage)
+            rawImage.texture = _tex;
+
+        if (aspectRatioFitter)
+            aspectRatioFitter.aspectRatio = (float)_tex.width / _tex.height;
+
+        RenderTexture.active = prevRt;
+    }
+}
---
+++ Assets/Videolab/Scripts/DisplayManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayManager : MonoBehaviour {
+
+    public string tagName = "MainCamera";
+
+	[HideInInspector]
+	public ExternalDisplay externalDisplay;
+
+	GameObject _externalCamera;
+
+	void Update()
+	{
+        GameObject mainCamera = GameObject.FindGameObjectWithTag(tagName);
+
+		if (mainCamera != _externalCamera)
+		{
+			if (mainCamera != null)
+			{
+                if (!mainCamera.GetComponent<ExternalDisplay>())
+				    mainCamera.AddComponent<ExternalDisplay>();
+			}
+		
+			_externalCamera = mainCamera;
+		}
+	}
+}
