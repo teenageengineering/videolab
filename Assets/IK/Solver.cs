f7fc5faa75833c8366a7140f7233ef3198b096c5
Finn Ericson <finn@Finns-MacBook-Pro-2.local>
2018-06-08 17:35:13 +0200

    Add basic IK solver

---
+++ Assets/IK/Solver.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IK 
+{
+	public class Solver : MonoBehaviour 
+	{
+        public Transform rootJoint;
+        public Transform endEffector;
+        public Transform target;
+
+        public float tolerance = 0;
+        public int maxIterations = 4;
+
+        public float blendWeight = 1;
+
+		void LateUpdate()
+		{
+            if (!endEffector.IsChildOf(rootJoint))
+            {
+                Debug.Log("[IK.Solver] End effector is not a child of root joint.");
+
+                return;
+            }
+
+            List<Vector3> solution = new List<Vector3>();
+
+            Vector3 startPos = rootJoint.position;
+            Vector3 targetPos = target.position;
+            
+            float chainLen = 0;
+            List<float> ds = new List<float>();
+            Transform joint;
+
+            joint = rootJoint;
+            while (true)
+            {
+                solution.Add(joint.position);
+
+                float d = Vector3.Magnitude(joint.localPosition);
+                ds.Add(d);
+                chainLen += d;
+
+                if (joint == endEffector)
+                    break;
+                
+                joint = joint.GetChild(0);
+            }
+
+            // target unreachable
+            if (chainLen < Vector3.Distance(targetPos, rootJoint.position))
+                targetPos = rootJoint.position + chainLen * Vector3.Normalize(targetPos - rootJoint.position);
+
+            for (int k = 0; k < maxIterations; k++)
+            {
+                //inward
+                solution[solution.Count - 1] = targetPos;
+                for (int i = solution.Count - 1; i > 0; i--)
+                {
+                    Vector3 v = Vector3.Normalize(solution[i] - solution[i - 1]);
+                    solution[i - 1] = solution[i] - ds[i] * v;
+                }
+
+                //outward
+                solution[0] = startPos;
+                for (int i = 1; i < solution.Count; i++)
+                {
+                    Vector3 v = Vector3.Normalize(solution[i] - solution[i - 1]);
+                    solution[i] = solution[i - 1] + ds[i] * v;
+                }
+
+                if (Vector3.Distance(solution[solution.Count - 1], targetPos) < (tolerance + 0.001))
+                    break;
+            }
+
+            joint = rootJoint;
+            for (int i = 1; i < solution.Count; i++)
+            {
+                joint = joint.GetChild(0);
+                joint.position = Vector3.Lerp(joint.position, solution[i], blendWeight);
+            }
+        }
+	}
+}
