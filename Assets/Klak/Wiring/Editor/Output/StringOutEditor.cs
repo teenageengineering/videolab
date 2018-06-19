5da502329fb1c0cbd3978d82a12d614ada9b6f05
finn <finn@teenage.engineering>
2018-06-19 10:17:24 +0200

    Add Klak StringOut

---
+++ Assets/Klak/Wiring/Editor/Output/StringOutEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Wiring
+{
+    [CustomEditor(typeof(StringOut))]
+    public class StringOutEditor : GenericOutEditor<string>
+    {
+        SerializedProperty _format;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            _format = serializedObject.FindProperty("_format");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(_format);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
