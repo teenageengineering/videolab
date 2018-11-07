7b8f18aa10c7b3228bd0406c58ac8ce869fe2fae
finn <finn.ericson@gmail.com>
2018-11-07 17:03:56 +0100

    Add RumbleOut Klak node

---
+++ Assets/Klak/Wiring/Output/RumbleOut.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Output/Rumble Out")]
+    public class RumbleOut : NodeBase
+    {
+        #region Node I/O
+
+        [Inlet]
+		public void Vibrate()
+		{
+			Handheld.Vibrate();
+		}
+
+        #endregion
+	}
+}
