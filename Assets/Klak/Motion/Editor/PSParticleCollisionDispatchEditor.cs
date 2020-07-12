6e55cd130f2ae985271789335629674d48ff1ea6
MysticalMonkeys <64361090+MysticalMonkeys@users.noreply.github.com>
2020-07-13 00:40:42 +0530

    Particle Collision Dispatch Editors

---
+++ Assets/Klak/Motion/Editor/PSParticleCollisionDispatchEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Motion
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(PSParticleCollisionDispatch))]
+    public class PSParticleCollisionDispatchEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertiesExcluding(serializedObject, new string[] {"m_Script"});
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Klak/Motion/Editor/ParticleCollisionDispatchEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Motion
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ParticleCollisionDispatch))]
+    public class ParticleCollisionDispatchEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertiesExcluding(serializedObject, new string[] { "m_Script" });
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
