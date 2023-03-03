16a4a316ba0c6bffeb990c6822c433e4ec7da02c
MysticalMonkeys <64361090+MysticalMonkeys@users.noreply.github.com>
2023-03-03 18:12:01 +0800

    Add New Klak Node: Float Gate
    New Klak node similar to the Bang Filter node, but for float values instead of bangs. 
    The float input value gets passed on only if the gate is opened.

---
+++ Assets/Klak/Wiring/Filter/FloatGate.cs
@@ -0,0 +1,47 @@
+﻿// Klak - Creative coding library for Unity
+// https://github.com/keijiro/Klak
+
+using UnityEngine;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Filter/Float Gate")]
+    public class FloatGate : NodeBase
+    {
+        #region Editable properties
+
+        [SerializeField]
+        bool _state;
+
+        #endregion
+
+        #region Node I/O
+
+        [Inlet]
+        public float input
+        {
+            set
+            {
+                if (_state) _outputEvent.Invoke(value);
+            }
+        }
+        
+
+        [Inlet]
+        public void Open()
+        {
+            _state = true;
+        }
+
+        [Inlet]
+        public void Close()
+        {
+            _state = false;
+        }
+
+        [SerializeField, Outlet]
+        FloatEvent _outputEvent = new FloatEvent();
+
+        #endregion
+    }
+}
