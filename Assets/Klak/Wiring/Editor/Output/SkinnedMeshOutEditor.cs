23a25614bcc73ca80579097a2c9db6af901a13cc
Finn Ericson <finn.ericson@gmail.com>
2023-09-01 14:57:10 +0200

    SkinnedMeshOut node

---
+++ Assets/Klak/Wiring/Editor/Output/SkinnedMeshOutEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Wiring
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(SkinnedMeshOut))]
+    public class SkinnedMeshOutEditor : Editor
+    {
+        SerializedProperty _skinnedMesh;
+        SerializedProperty _blendShapeIndex;
+
+        void OnEnable()
+        {
+            _skinnedMesh = serializedObject.FindProperty("_skinnedMesh");
+            _blendShapeIndex = serializedObject.FindProperty("_blendShapeIndex");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_skinnedMesh);
+
+            SkinnedMeshRenderer skinnedMesh = _skinnedMesh.objectReferenceValue as SkinnedMeshRenderer;
+            if (skinnedMesh)
+            {
+                string[] blendShapeNames = GetBlendShapeNames(skinnedMesh.gameObject);
+                _blendShapeIndex.intValue = EditorGUILayout.Popup("Blend Shape Index", _blendShapeIndex.intValue, blendShapeNames);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public string[] GetBlendShapeNames(GameObject obj)
+        {
+            SkinnedMeshRenderer head = obj.GetComponent<SkinnedMeshRenderer>();
+            Mesh m = head.sharedMesh;
+            string[] names = new string[m.blendShapeCount];
+            for (int i = 0; i < m.blendShapeCount; i++)
+                names[i] = m.GetBlendShapeName(i);
+
+            return names;
+        }
+    }
+}
---
+++ Assets/Klak/Wiring/Output/SkinnedMeshOut.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Output/Component/Skinned Mesh Out")]
+    public class SkinnedMeshOut : NodeBase
+    {
+        #region Editable properties
+
+        [SerializeField]
+        SkinnedMeshRenderer _skinnedMesh;
+
+        [SerializeField]
+        int _blendShapeIndex;
+
+        #endregion
+
+        int blendShapeCount;
+
+        #region Node I/O
+
+        [Inlet]
+        public float weight {
+            set {   
+                if (!enabled || _skinnedMesh == null) return;
+                float w = Mathf.Clamp(value * 100, 0, 100);
+                _skinnedMesh.SetBlendShapeWeight(_blendShapeIndex, w);
+            }
+        }
+
+        #endregion
+    }
+}
